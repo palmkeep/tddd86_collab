82b29355e8ad26fef7aeb8f6d6d3a461c5e06652
Elise Lång <elila090@parlomba1.ida.liu.se>
2017-09-08 16:40:24 +0200

    Labb 2 inlagd, wordchain & evilhangman

---
+++ wordchain/src/wordchain.cpp
@@ -0,0 +1,32 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <queue>
+#include <stack>
+using namespace std;
+
+const string ALPHABET  = "abcdefghijklmnopqrstuvwxyz";
+/*
+void wordChain(string word1, string word2){
+    queue<stack<string>> queueOfStacks;
+    stack
+}
+*/
+
+int main() {
+    cout << "Welcome to TDDD86 Word Chain." << endl;
+    cout << "If you give me two English words, I will transform the" << endl;
+    cout << "first into the second by changing one letter at a time." << endl;
+    cout << endl;
+
+    cout << "Please type two words: ";
+    string word1, word2;
+    cin >> word1 >> word2;
+    cout << "Chain from " << word1 << " back to " << word2 << ":" << endl;
+
+    // wordChain(word1, word2);
+
+    // TODO: Finish the program!
+
+    return 0;
+}
