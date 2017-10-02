d7fb3c448798a5aa58df1ff2a51dfb49f8a90e26
Elise Lång <elila090@li12-8.ida.liu.se>
2017-10-02 15:41:54 +0200

    finished lab 2 + E1 + E2

--- evilhangman/src/evilhangman.cpp
+++ evilhangman/src/evilhangman.cpp
@@ -14,8 +14,29 @@ using namespace std;
 const string ALPHABET  = "abcdefghijklmnopqrstuvwxyz";
 
 
+// Reads in all the words of length $wordLength from the dictionary from dictionary.txt.
+set<string> initDictionary(const u_int wordLength)
+{
+    ifstream inDictionary;
+    inDictionary.open("dictionary.txt");
+
+    set<string> dictionary;
+    string dictWord;
+    while(!inDictionary.eof())
+    {
+        inDictionary >> dictWord;
+        if (dictWord.size() == wordLength)
+        {
+            dictionary.insert(dictWord);
+        }
+    }
+
+    return dictionary;
+}
+
+
 //Returns the input string along with an array that contains the indicies of all occurences of inout guess.
-pair<string, vector<int>> findGuessInWord(const string& word, const char& guess)
+pair< string, vector<int> > findGuessInWord(const string& word, const char& guess)
 {
     vector<int> indexGuessOccurence;
     int lengthWord = word.size();
@@ -32,8 +53,9 @@ pair<string, vector<int>> findGuessInWord(const string& word, const char& guess)
 
 
 // Find and return the largest dictionary
-set<string> biggestDict(const char& guess, const set<string>& dictionary)
+vector<int> findBiggestDictionary(const char guess, set<string>& dictionary)
 {
+    // Creates a stack containing pairs of words and the indices of the guessed letter
     stack< pair<string, vector<int>> > wordGuessOccurencePairs;
     for (set<string>::iterator word = dictionary.begin(); word != dictionary.end(); ++word)
     {
@@ -41,143 +63,73 @@ set<string> biggestDict(const char& guess, const set<string>& dictionary)
     }
 
 
-    map< int, pair<string, vector<int>> > dictSet;  // ERROR: need to fix .insert() or similar for map datatype
+    // Divides the pairs in the stack generated into sets where all words in a set share the same
+    // index vector
+    map< vector<int>, set<string> > dictSet;
     while ( !wordGuessOccurencePairs.empty() )
     {
         pair< string, vector<int> > wordGuessOccurence = wordGuessOccurencePairs.top();
-
         string word = wordGuessOccurence.first;
         vector<int> guessIndex = wordGuessOccurence.second;
-        int correctGuessInWord = (wordGuessOccurence.second).size();
-        if (dictSet.find(correctGuessInWord))
+        dictSet.find(guessIndex);
+
+        set<string> &newDictionary = dictSet[guessIndex];
+        newDictionary.insert(word);
+
+        wordGuessOccurencePairs.pop();
+    }
+
+    // Picks out the largest and worst (one that gives the feweset letters in the constraint) set of strings
+    int sizeBiggestDict = 0;
+    int biggestBonusPoints = 0;
+    vector<int> newConstraint;
+    set<string> biggestDictionary;
+    const string leastCommonLetters = "qjxz";
+    for (map< vector<int>, set<string> >::iterator dict = dictSet.begin(); dict != dictSet.end(); ++dict)
+    {
+
+        int bonusPoints = 0;
+        for (set<string>::const_iterator word = (dict->second).begin(); word != (dict->second).end(); ++word) // E2: Calculate bonus for having hard to guess letters in the dictionary
         {
-            dictSet.insert(correctGuessInWord, wordGuessOccurence);
-        } // WHAT IF NOT?
-    }
-
-    int biggestDict;
-    for (map<int, pair<string, vector<int>> >::iterator dict = dictSet.begin(); dict != dictSet.end(); ++dict)
-    {
-        if (*dict.second)
-    }
-}
-
-
-//Reads in all the words of length $wordLength from the dictionary from dictionary.txt.
-set<string> initDictionary(const u_int wordLength)
-{
-    ifstream inDictionary;
-    inDictionary.open("dictionary.txt");
-
-    set<string> dictionary;
-    string dictWord;
-    set<string> dictionary;
-    while(!inDictionary.eof())
-    {
-        inDictionary >> dictWord;
-        if (dictWord.size() == wordLength)
-        {
-            dictionary.insert(dictWord);
-        }
-    }
-
-    return dictionary;
-}
-
-// Singles out the bad words from dic.
-set<string> updateDictionary(const set<string>& dictionary, const string& wordConstraints)
-{
-
-    // Creates a Queue of pairs containing each letter in the constraint alongside that letters index in the word
-    queue< pair<char, int> > constraintsQueue;
-    for(string::const_iterator constIt = wordConstraints.begin(); constIt != wordConstraints.end(); ++constIt)
-    {
-        if (*constIt != '_')
-        {
-            constraintsQueue.push(pair<char, int>(*constIt, distance(wordConstraints.begin(), constIt) ));
-        }
-    }
-
-    int numConstraints = constraintsQueue.size();
-
-    cout << "Constraints" << endl;
-    for(int i = 0; i < numConstraints; i++)
-    {
-        pair<char,int> currentConstraint = constraintsQueue.front();
-        cout << currentConstraint.first << ' ' << currentConstraint.second << endl;
-
-        constraintsQueue.push(constraintsQueue.front());
-        constraintsQueue.pop();
-    }
-
-    cout << endl; // Huh?
-
-    set<string> newDictionary;
-    for(set<string>::const_iterator setIt = dictionary.begin(); setIt != dictionary.end(); ++setIt)
-    {
-        pair<char, int> currentConstraint = constraintsQueue.front();
-        string currentWord = *word;
-
-        bool fitsConstraint = true;
-        int i = 0;
-        while(i < numConstraints && fitsConstraint)
-        {
-            /* DEBUG MODE
-            cout << endl;
-            cout << currentConstraint.first << ' ' << currentConstraint.second << endl;
-            cout << currentWord << endl;
-            */
-            if(currentWord.at(currentConstraint.second) != currentConstraint.first)
+            for (string::const_iterator stringIt = leastCommonLetters.begin(); stringIt != leastCommonLetters.end(); ++stringIt)
             {
-                fitsConstraint = false;
+                if ((*word).find(*stringIt) != string::npos)
+                {
+                    bonusPoints += 1;
+                }
             }
-
-            constraintsQueue.push(constraintsQueue.front());
-            constraintsQueue.pop();
-            currentConstraint = constraintsQueue.front();
-            i += 1;
         }
-        if (fitsConstraint)
+        if ( (dictSet.at(dict->first)).size() + bonusPoints >= sizeBiggestDict + biggestBonusPoints ) // E2: will be handled by the extra weight given by bonusPoints
         {
-            newDictionary.insert(currentWord);
-        }
-    }
-
-    return newDictionary;
-}
-
-
-// Gives back the largest dictionary given a guess from the user.
-// Updates the constraint to match the new dictionary.
-set<string> findLargestDictionary(const set<string> dictionary, const char& guess, string& constraint)
-{
-    set<string> largestDictionary;
-
-    return largestDictionary; 
-}
-
-
-// To be constructed!!!
-set<string> applyConstraint(const set<string>& prevDict, string constraint, char guess)
-{
-
-    int lengthWord = (*prevDict.begin()).size(); // COMMENT GOES HERE
-    set<string> newDict;
-    for (int i = 0; i < lengthWord; i++)
-    {
-        for (set<string>::iterator dictIt = prevDict.begin(); dictIt != prevDict.end(); ++dictIt)
-        {
-            if ((*dictIt)[i] == guess)
+            if ( (dictSet.at(dict->first)).size() + bonusPoints == sizeBiggestDict + biggestBonusPoints ) // E1: will be handled by this and the next if-statement
             {
-
+                if ( (dict->first).size() < newConstraint.size() )
+                {
+                    biggestDictionary = dictSet.at(dict->first);
+                    newConstraint = dict->first;
+                    sizeBiggestDict = dictSet.at(dict->first).size();
+                    biggestBonusPoints = bonusPoints;
+                }
+            }
+            else
+            {
+                biggestDictionary = dictSet.at(dict->first);
+                newConstraint = dict->first;
+                sizeBiggestDict = dictSet.at(dict->first).size();
+                biggestBonusPoints = bonusPoints;
             }
         }
     }
-    return newDict;
+
+    dictionary = biggestDictionary;
+    return newConstraint;
+    //return make_pair(dictionary, newConstraint);
 }
 
 
-// Main loop
+
+// Main loop handles user input and updates the dictionary of possible words accordingly
+// quits when the player has guessed the correct word or ran out of guesses
 int main()
 {
     cout << "Welcome to Hangman." << endl;
@@ -193,28 +145,79 @@ int main()
  
     set<string> dictionary = initDictionary(wordLength);
 
+
     string constraint;
+    int numBlanks = 0;
     for (int i = 0; i < wordLength; i++)
     {
         constraint += '_';
+        numBlanks += 1;
     }
     cout << constraint << endl;
 
 
-    for (int i = 0; i < numGuesses; i++)
+
+    while (numGuesses > 0)
     {
         cout << "Guess a new character: " << endl;
         char guess;
         cin >> guess;
-        dictionary = applyConstraint(dictionary, constraint, guess);
+
+        if (numGuesses == 1)    // E1: Handles the last guess without E2 interfering
+        {
+            for (set<string>::const_iterator word = dictionary.begin(); word != dictionary.end(); ++word)
+            {
+                if ((*word).find(guess) == string::npos)
+                {
+                    cout << "YOU LOST!" << endl;
+                    return 0;
+                }
+            }
+        }
+
+        vector<int> newConstraint = findBiggestDictionary(guess, dictionary);
+
+
+        for (vector<int>::iterator it = newConstraint.begin(); it != newConstraint.end(); ++it)
+        {
+            constraint.at(*it) = guess;
+        }
+        cout << constraint << endl;
+
+        int numBlanks = 0;
+        for (string::iterator it = constraint.begin(); it != constraint.end(); ++it)
+        {
+            if (*it == '_')
+            {
+                numBlanks += 1;
+            }
+        }
+        if (numBlanks == 0)
+        {
+            cout << "YOU WIN!" << endl;
+            break;
+        }
+
+
+        if (newConstraint.size() == 0)
+        {
+            numGuesses -= 1;
+        }
     }
 
+    if (numGuesses == 0)
+    {
+        cout << "YOU LOST!" << endl;
+    }
+
+    /* DEBUG
     cout << endl << endl;
     cout << dictionary.size() << endl;
     for(set<string>::iterator setIt = dictionary.begin(); setIt != dictionary.end(); ++setIt)
     {
         cout << *setIt << endl;
     }
+    */
     
 
     return 0;
